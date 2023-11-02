e2a47af4c1b604a4ad409cb260849285b829ca61
Matias <matias_arenhard@hotmail.com>
2023-11-02 17:41:12 -0300

    add strong params

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -3,8 +3,17 @@
 module Api
   module V1
     class TransactionsController < ApplicationController
+      before_action :transaction_params, only: [:create]
+
       def create
         render json: {}, status: :ok
+      end
+
+      private
+
+      def transaction_params
+        params.permit(:transaction_id, :merchant_id, :user_id, :card_number,
+                      :transaction_date, :transaction_amount, :device_id, :has_cbk)
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if User.find_by(email: 'admin@cloudwalk.com').nil?
+  user = User.create(email: 'admin@cloudwalk.com', password: 'manager')
+  puts "Use this token: #{user.authentication_token}"
+end
